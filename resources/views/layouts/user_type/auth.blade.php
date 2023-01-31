db498d0b3a29238ebec509702b10dd9bc0952a15
Inacio <inacio.kawala@gmail.com>
2023-01-31 10:10:51 +0100

    Navbar conformado

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -3,18 +3,18 @@
 @section('auth')
 
 
-    @if(\Request::is('static-sign-up')) 
+    @if(\Request::is('static-sign-up'))
         @include('layouts.navbars.guest.nav')
         @yield('content')
         @include('layouts.footers.guest.footer')
-    
-    @elseif (\Request::is('static-sign-in')) 
+
+    @elseif (\Request::is('static-sign-in'))
         @include('layouts.navbars.guest.nav')
             @yield('content')
         @include('layouts.footers.guest.footer')
-    
+
     @else
-        @if (\Request::is('rtl'))  
+        @if (\Request::is('rtl'))
             @include('layouts.navbars.auth.sidebar-rtl')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
                 @include('layouts.navbars.auth.nav-rtl')
@@ -23,15 +23,14 @@
                     @include('layouts.footers.auth.footer')
                 </div>
             </main>
-
-        @elseif (\Request::is('profile'))  
+        @elseif (\Request::is('profile'))
             @include('layouts.navbars.auth.sidebar')
             <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
                 @include('layouts.navbars.auth.nav')
                 @yield('content')
             </div>
 
-        @elseif (\Request::is('virtual-reality')) 
+        @elseif (\Request::is('virtual-reality'))
             @include('layouts.navbars.auth.nav')
             <div class="border-radius-xl mt-3 mx-3 position-relative" style="background-image: url('../assets/img/vr-bg.jpg') ; background-size: cover;">
                 @include('layouts.navbars.auth.sidebar')
@@ -55,6 +54,6 @@
         @include('components.fixed-plugin')
     @endif
 
-    
+
 
 @endsection
